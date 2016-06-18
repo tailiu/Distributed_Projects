bf1bd0acb8b24481055a03850e8230b13b3cb296
Tai Liu <tl67@nyu.edu>
2016-06-18 13:50:09 +0400

    rm useless dir

--- app_lib/three.js
+++
@@ -1,5 +0,0 @@
-var Util = require('./util');
-var util = new Util();
-
-
-util.createDHTSeed('127.0.0.1', 49151, 'hehehe/db');
--- app_lib/two.js
+++
@@ -1,7 +0,0 @@
-var Group = require('./group');
-var group = new Group();
-
-group.joinGroup('groupName', '127.0.0.1', 'hehe/db', [
-    { address: 'localhost', port: 49151 }
-  ]);
-
